54826e550640e081a8a691124baa1d63bd445b5d
dongha-byun <byunsw4@naver.com>
2026-01-06 21:21:48 +0900

    feat: menu 조회 수정

--- src/api/menu/menu.ts
+++ src/api/menu/menu.ts
@@ -2,7 +2,7 @@ import { apiService } from "../axiosInstance";
 
 export const menuApi = {
     list: async () => {
-        const response = await apiService.get("/api/v1/menus");
+        const response = await apiService.get("/api/v1/menus/auth");
         console.log("menuService:list ", response);
         return response.data.data;
     },
