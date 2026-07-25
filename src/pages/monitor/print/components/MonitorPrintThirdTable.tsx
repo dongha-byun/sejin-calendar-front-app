20d502f8d3eefb6232328781bee0e459c8879d12
dongha-byun <byunsw4@naver.com>
2026-07-25 12:15:58 +0900

    feat: 인쇄소별 용지재고조회 컬럼 순서 변경

--- src/pages/monitor/print/components/MonitorPrintThirdTable.tsx
+++ src/pages/monitor/print/components/MonitorPrintThirdTable.tsx
@@ -11,13 +11,13 @@ function DetailTable({ data }: { data: PrintCommandDetailDto[] }) {
         <table className="table-auto border text-sm whitespace-nowrap">
             <thead>
                 <tr>
-                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄전표번호</th>
-                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄소</th>
-                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄일</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">무게</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">지질</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">규격</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">사용량</th>
+                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄소</th>
+                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄전표번호</th>
+                    <th className="border px-2 py-1 sticky top-0 bg-gray-200">인쇄일</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">모델#</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">모델명</th>
                     <th className="border px-2 py-1 sticky top-0 bg-gray-200">부수</th>
@@ -27,13 +27,13 @@ function DetailTable({ data }: { data: PrintCommandDetailDto[] }) {
             <tbody>
             {data.map((s, idx) => (
                 <tr key={s.commandPrintId} className={idx % 2 === 0 ? "bg-white" : "bg-gray-50"}>
-                    <td className="border px-2 py-1">{s.statementNum}</td>
-                    <td className="border px-2 py-1">{s.printCompanyName}</td>
-                    <td className="border px-2 py-1">{s.issueDate}</td>
                     <td className="border px-2 py-1 text-right">{s.weight}</td>
                     <td className="border px-2 py-1">{s.properties}</td>
                     <td className="border px-2 py-1">{s.standard}</td>
                     <td className="border px-2 py-1 text-right">{s.amount?.toLocaleString() ?? '-'}</td>
+                    <td className="border px-2 py-1">{s.printCompanyName}</td>
+                    <td className="border px-2 py-1">{s.statementNum}</td>
+                    <td className="border px-2 py-1">{s.issueDate}</td>
                     <td className="border px-2 py-1">{s.modelNum}</td>
                     <td className="border px-2 py-1">{s.modelName}</td>
                     <td className="border px-2 py-1 text-right">{s.totalCount.toLocaleString()}</td>
